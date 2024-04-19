1ef787f0afaeb64ba213e70cc78011dd24fe3171
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-04-19 13:37:14 +0200

    useDispatch

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,33 +3,33 @@ import { FaUser } from 'react-icons/fa';
 import { FaPhone } from 'react-icons/fa';
 import { MdDeleteForever } from 'react-icons/md';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
 
-const Contact = ({
-  contact: { id, name, phone },
-  deletingContact,
-  setter,
-}) => {
+const Contact = ({ contact: { id, name, phone }, setter }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = () => {
+    dispatch(deleteContact(id));
+    setter('');
+  };
+
   return (
     <div className={css.contact}>
-        <div>
+      <div>
         <div className={css.contactInfo}>
-            <FaUser className={css.contactIcon} />
-            <h2 className={css.name}>{name}</h2>
+          <FaUser className={css.contactIcon} />
+          <h2 className={css.name}>{name}</h2>
         </div>
-    
         <div className={css.contactInfo}>
-            <FaPhone className={css.contactIcon} />
-            <p className={css.phone}>{phone}</p>
+          <FaPhone className={css.contactIcon} />
+          <p className={css.phone}>{phone}</p>
         </div>
-        </div>
-        
-        <button
+      </div>
+      <button
         className={css.button}
         type="button"
-        onClick={() => {
-          deletingContact(id);
-          setter('');
-        }}
+        onClick={handleDeleteContact}
       >
         <MdDeleteForever className={css.icon} />
       </button>
@@ -38,9 +38,8 @@ const Contact = ({
 };
 
 Contact.propTypes = {
-    contact: PropTypes.object.isRequired,
-    deletingContact: PropTypes.func.isRequired,
-    setter: PropTypes.func.isRequired,
-  };
+  contact: PropTypes.object.isRequired,
+  setter: PropTypes.func.isRequired,
+};
 
 export default Contact;
